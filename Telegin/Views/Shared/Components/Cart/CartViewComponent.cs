7c9ed6ed0d3e7d04a791f92c01ffaa61fed4e547
Dogmax104 <maxpesfix@gmail.com>
2024-11-17 16:05:15 +0300

    addition 17.11.24

---
+++ Telegin/Views/Shared/Components/Cart/CartViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+
+
+public class CartViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        var cart = new CartModel
+        {
+            Items = new List<CartItemModel>
+            {
+                new CartItemModel{Name="Трактор",Price=126},
+                new CartItemModel{Name="Автомобиль",Price=70}
+            }
+        };
+         return View(cart);
+    }
+}
+
+public class CartModel
+{
+    public List<CartItemModel>? Items{get; set;}
+}
+public class CartItemModel
+{
+    public string? Name { get; set; }
+    public decimal Price { get; set; }
+}
+
